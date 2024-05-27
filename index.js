bf2457f06305d6743d6c3b44322e69e1efc61bd3
tonyeessome <tonyeabel2@gmail.com>
2024-05-28 00:17:54 +0200

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { exec } = require('child_process');
 // Configuration
 const PORT = process.env.PORT || 3000;
 const SECRET = 'tonye';
-const REPO_PATH = '/c/Users/tonye/webhook';
+const REPO_PATH = 'https://github.com/tonyeessome/webhook';
 const BRANCH = 'main';  // Branch you want to track
 
 // Initialize express app
